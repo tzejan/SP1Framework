b649d6249904e1a2ddeb33658bf44cf56c78ba43
uncannycannary <cyboryxmen@yahoo.com>
2014-08-21 14:55:34 +0800

    Revert "Stuff"
    This reverts commit fffa12d87290911f3ae0b313b25e4e7a6e892125.
    
    Conflicts:
    	SP1Framework/game.cpp
    	SP1Framework/game.h

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -5,19 +5,25 @@
 #include "Framework\console.h"
 #include <iostream>
 #include <iomanip>
+<<<<<<< HEAD
 #include "Graphics.h"
 #include "TableFlip.h"
 <<<<<<< HEAD
+=======
+>>>>>>> parent of fffa12d... Stuff
 
 double elapsedTime;
 double deltaTime;
 bool keyPressed[K_COUNT];
 COORD charLocation;
 COORD consoleSize;
+<<<<<<< HEAD
 Graphics TableFlip;
 
 =======
 #include "Scream.h"
+=======
+>>>>>>> parent of fffa12d... Stuff
 
 double elapsedTime;
 double deltaTime;
@@ -31,27 +37,54 @@ gamestate state = TABLE_FLIP;
 void init()
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 	scream = NULL;
 >>>>>>> origin/master
+=======
+    // Set precision for floating point output
+    std::cout << std::fixed << std::setprecision(3);
+
+    // Get console width and height
+    CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */     
+
+    /* get the number of character cells in the current buffer */ 
+    GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ), &csbi );
+    consoleSize.X = csbi.srWindow.Right + 1;
+    consoleSize.Y = csbi.srWindow.Bottom + 1;
+
+    // set the character to be in the center of the screen.
+    charLocation.X = consoleSize.X / 2;
+    charLocation.Y = consoleSize.Y / 2;
+
+>>>>>>> parent of fffa12d... Stuff
     elapsedTime = 0.0;
 }
 
 void shutdown()
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+    // Reset to white text on black background
+	colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+>>>>>>> parent of fffa12d... Stuff
 }
 
 void getInput()
 {    
+    keyPressed[K_UP] = isKeyPressed(VK_UP);
     keyPressed[K_DOWN] = isKeyPressed(VK_DOWN);
     keyPressed[K_LEFT] = isKeyPressed(VK_LEFT);
     keyPressed[K_RIGHT] = isKeyPressed(VK_RIGHT);
     keyPressed[K_ESCAPE] = isKeyPressed(VK_ESCAPE);
+<<<<<<< HEAD
 	keyPressed[K_ENTER] = isKeyPressed(VK_RETURN);
 =======
 >>>>>>> origin/master
+=======
+>>>>>>> parent of fffa12d... Stuff
 }
 
 void update(double dt)
@@ -61,7 +94,27 @@ void update(double dt)
     elapsedTime += dt;
     deltaTime = dt;
 
-	updateTableFlip();
+    // Updating the location of the character based on the key press
+    if (keyPressed[K_UP] && charLocation.Y > 0)
+    {
+        Beep(1440, 30);
+        charLocation.Y--; 
+    }
+    if (keyPressed[K_LEFT] && charLocation.X > 0)
+    {
+        Beep(1440, 30);
+        charLocation.X--; 
+    }
+    if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1)
+    {
+        Beep(1440, 30);
+        charLocation.Y++; 
+    }
+    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
+    {
+        Beep(1440, 30);
+        charLocation.X++; 
+    }
 
     // quits the game if player hits the escape key
     if (keyPressed[K_ESCAPE])
@@ -103,10 +156,47 @@ void update(double dt)
 void render()
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	drawTableFlip(&TableFlip);
 	TableFlip.updateconsole();
 =======
 	//Updates console to current screen
 	console.updateconsole();    
 >>>>>>> origin/master
+=======
+    // clear previous screen
+    colour(0x0F);
+    cls();
+
+    //render the game
+
+    //render test screen code (not efficient at all)
+    const WORD colors[] =   {
+	                        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
+	                        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
+	                        };
+	
+	for (int i = 0; i < 12; ++i)
+	{
+		gotoXY(3*i,i+1);
+		colour(colors[i]);
+		std::cout << "WOW";
+	}
+
+    // render time taken to calculate this frame
+    gotoXY(70, 0);
+    colour(0x1A);
+    std::cout << 1.0 / deltaTime << "fps" << std::endl;
+  
+    gotoXY(0, 0);
+    colour(0x59);
+    std::cout << elapsedTime << "secs" << std::endl;
+
+    // render character
+    gotoXY(charLocation);
+    colour(0x0C);
+    std::cout << (char)1;
+
+    
+>>>>>>> parent of fffa12d... Stuff
 }
