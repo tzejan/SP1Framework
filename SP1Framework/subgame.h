185dfdad265c3ac428bc3ebb21657885407e0c66
shingliya <damianivan@hotmail.com>
2014-08-22 09:37:50 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ SP1Framework/subgame.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Graphics.h"
+#include "Framework\timer.h"
+#include "Directory.h"
+#include <fstream>
+#include "Framework\console.h"
+
+void initialisesubdrawings();
+void playsubgamemain(Graphics*);
+void playsubcontrolsmechanics(Graphics*);
