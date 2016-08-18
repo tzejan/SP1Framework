06f7cfd6758f090e7108b73f64e4ab4aa90a8a37
Jean <lzyjeann@gmail.com>
2016-08-18 14:12:06 +0800

    Fixed the block  disppearing at door issue

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -301,9 +301,8 @@ void moveCharacter()
 	}
 	if (g_abKeyPressed[K_RIGHT] && g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1)
 	{
-		if (map[(g_sChar.m_cLocation.Y) - (25 - mapSizeHeight)][(g_sChar.m_cLocation.X + 1) - (90 - mapSizeWidth)] != (char)219)
+		if (map[(g_sChar.m_cLocation.Y) - (25 - mapSizeHeight)][(g_sChar.m_cLocation.X + 1) - (90 - mapSizeWidth)] != (char)219) 
 		{
-
 			if (map[(g_sChar.m_cLocation.Y) - (25 - mapSizeHeight)][(g_sChar.m_cLocation.X + 1) - (90 - mapSizeWidth)] != (char)186)
 			{
 				if ((map[(g_sChar.m_cLocation.Y) - (25 - mapSizeHeight)][(g_sChar.m_cLocation.X + 1) - (90 - mapSizeWidth)] == (char)254) && (map[(g_sChar.m_cLocation.Y) - (25 - mapSizeHeight)][(g_sChar.m_cLocation.X + 2) - (90 - mapSizeWidth)] != (char)219)) //if there is a block and nothing is blocking it
@@ -350,52 +349,52 @@ void moveCharacter()
 	}
 
 	//Doors:
-	if (map[6][106] == (char)254) //Door1 
+	if (map[6][106] == (char)254 && map[5][82] != (char) 254 && map[5][83] != (char)254) //Door1 
 	{
 		map[5][82] = ' ';
 		map[5][83] = ' ';
 	}
 
-	if (map[11][119] == (char)254) //Door2
+	if (map[11][119] == (char)254 && map[24][96] != (char)254 && map[24][97] != (char)254) //Door2
 	{
 		map[24][96] = ' ';
 		map[24][97] = ' ';
 	}
 
-	if (map[21][85] == (char)254) //Door3
+	if (map[21][85] == (char)254 && map[4][57] != (char)254 && map[4][58] != (char)254) //Door3
 	{
 		map[21][86] = ' ';
 		map[4][57] = ' ';
 		map[4][58] = ' ';
 	}
 
-	if (map[9][69] == (char)254) //Door4
+	if (map[9][69] == (char)254 && map[14][80] != (char)254 && map[14][81] != (char)254) //Door4
 	{
 		map[9][70] = ' ';
 		map[14][80] = ' ';
 		map[14][81] = ' ';
 	}
 
-	if (map[14][22] == (char)254) //Door5
+	if (map[14][22] == (char)254 && map[1][30] != (char)254 && map[1][31] != (char)254) //Door5
 	{
 		map[1][30] = ' ';
 		map[1][31] = ' ';
 	}
 
-	if (map[1][12] == (char)254) //Door6
+	if (map[1][12] == (char)254 && map[5][12] != (char)254 && map[5][13] != (char)254) //Door6
 	{
 		map[5][12] = ' ';
 		map[5][13] = ' ';
 	}
 
-	if (map[11][18] == (char)254) //Door7
+	if (map[11][18] == (char)254 && map[21][7] != (char)254 && map[21][8] != (char)254) //Door7
 	{
 		map[11][19] = ' ';
 		map[21][7] = ' ';
 		map[21][8] = ' ';
 	}
 
-	if (map[31][15] == (char)254) //Door8
+	if (map[31][15] == (char)254 && map[34][29] != (char)254 && map[34][30] != (char)254) //Door8
 	{
 		map[31][16] = ' ';
 		map[34][29] = ' ';
