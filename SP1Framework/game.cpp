d59bfe37916a8aab67c4f8a458d5e0123f6e87f6
SIM TZE JAN <sim_tze_jan@nyp.edu.sg>
2016-08-19 15:08:32 +0800

    Updated the framework to show how event input handlers work Added a keyboard input handler Added a mouse input handler Sample code on how to interpret input events.

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -9,12 +9,12 @@
 
 double  g_dElapsedTime;
 double  g_dDeltaTime;
-bool    g_abKeyPressed[K_COUNT];
+SKeyEvent g_skKeyEvent[K_COUNT];
+SMouseEvent g_mouseEvent;
 
 // Game specific variables here
 SGameChar   g_sChar;
-EGAMESTATES g_eGameState = S_SPLASHSCREEN;
-double  g_dBounceTime; // this is to prevent key bouncing, so we won't trigger keypresses more than once
+EGAMESTATES g_eGameState = S_SPLASHSCREEN; // initial state
 
 // Console object
 Console g_Console(80, 25, "SP1 Framework");
@@ -29,8 +29,7 @@ Console g_Console(80, 25, "SP1 Framework");
 void init( void )
 {
     // Set precision for floating point output
-    g_dElapsedTime = 0.0;
-    g_dBounceTime = 0.0;
+    g_dElapsedTime = 0.0;    
 
     // sets the initial state for the game
     g_eGameState = S_SPLASHSCREEN;
@@ -40,6 +39,10 @@ void init( void )
     g_sChar.m_bActive = true;
     // sets the width, height and the font name to use in the console
     g_Console.setConsoleFont(0, 16, L"Consolas");
+
+    // remember to set your keyboard handler, so that your functions can be notified of input events
+    g_Console.setKeyboardHandler(keyboardHandler);
+    g_Console.setMouseHandler(mouseHandler);
 }
 
 //--------------------------------------------------------------
@@ -58,24 +61,127 @@ void shutdown( void )
 }
 
 //--------------------------------------------------------------
-// Purpose  : Getting all the key press states
-//            This function checks if any key had been pressed since the last time we checked
-//            If a key is pressed, the value for that particular key will be true
+// Purpose  : Get all the console input events
+//            This function sets up the keyboard and mouse input from the console.
+//            We will need to reset all the keyboard status, because no events will be sent if no keys are pressed.
 //
-//            Add more keys to the enum in game.h if you need to detect more keys
-//            To get other VK key defines, right click on the VK define (e.g. VK_UP) and choose "Go To Definition" 
-//            For Alphanumeric keys, the values are their ascii values (uppercase).
+//            Remember to set the handlers for keyboard and mouse events.
+//            The function prototype of the handler is a function that takes in a const reference to the event struct
+//            and returns nothing. 
+//            void pfKeyboardHandler(const KEY_EVENT_RECORD&);
+//            void pfMouseHandlerconst MOUSE_EVENT_RECORD&);
 // Input    : Void
 // Output   : void
 //--------------------------------------------------------------
 void getInput( void )
+{
+    // resets all the keyboard events
+    memset(g_skKeyEvent, 0, K_COUNT * sizeof(*g_skKeyEvent));
+    // then call the console to detect input from user
+    g_Console.readConsoleInput();    
+}
+
+//--------------------------------------------------------------
+// Purpose  : This is the handler for the keyboard input. Whenever there is a keyboard event, this function will be called.
+//            Ideally, you should pass the key event to a specific function to handle the event.
+//            This is because in some states, some keys would be disabled. Hence, to reduce your code complexity, 
+//            it would be wise to split your keyboard input handlers separately.
+//            
+//            The KEY_EVENT_RECORD struct has more attributes that you can use, if you are adventurous enough.
+//
+//            In this case, we are not handling any keyboard event in the Splashscreen state
+//            
+// Input    : const KEY_EVENT_RECORD& keyboardEvent - reference to a key event struct
+// Output   : void
+//--------------------------------------------------------------
+void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent)
 {    
-    g_abKeyPressed[K_UP]     = isKeyPressed(VK_UP);
-    g_abKeyPressed[K_DOWN]   = isKeyPressed(VK_DOWN);
-    g_abKeyPressed[K_LEFT]   = isKeyPressed(VK_LEFT);
-    g_abKeyPressed[K_RIGHT]  = isKeyPressed(VK_RIGHT);
-    g_abKeyPressed[K_SPACE]  = isKeyPressed(VK_SPACE);
-    g_abKeyPressed[K_ESCAPE] = isKeyPressed(VK_ESCAPE);
+    switch (g_eGameState)
+    {
+    case S_SPLASHSCREEN: // don't handle anything for the splash screen
+        break;
+    case S_GAME: gameplayKBHandler(keyboardEvent); // handle gameplay keyboard event 
+        break;
+    }
+}
+
+//--------------------------------------------------------------
+// Purpose  : This is the handler for the mouse input. Whenever there is a mouse event, this function will be called.
+//            Ideally, you should pass the key event to a specific function to handle the event.
+//            This is because in some states, some keys would be disabled. Hence, to reduce your code complexity, 
+//            it would be wise to split your keyboard input handlers separately.
+//            
+//            For the mouse event, if the mouse is not moved, no event will be sent, hence you should not reset the mouse status.
+//            However, if the mouse goes out of the window, you would not be able to know either. 
+//
+//            The MOUSE_EVENT_RECORD struct has more attributes that you can use, if you are adventurous enough.
+//
+//            In this case, we are not handling any mouse event in the Splashscreen state
+//            
+// Input    : const MOUSE_EVENT_RECORD& mouseEvent - reference to a mouse event struct
+// Output   : void
+//--------------------------------------------------------------
+void mouseHandler(const MOUSE_EVENT_RECORD& mouseEvent)
+{    
+    switch (g_eGameState)
+    {
+    case S_SPLASHSCREEN: // don't handle anything for the splash screen
+        break;
+    case S_GAME: gameplayMouseHandler(mouseEvent); // handle gameplay mouse event
+        break;
+    }
+}
+
+//--------------------------------------------------------------
+// Purpose  : This is the keyboard handler in the game state. Whenever there is a keyboard event in the game state, this function will be called.
+//            
+//            Add more keys to the enum in game.h if you need to detect more keys
+//            To get other VK key defines, right click on the VK define (e.g. VK_UP) and choose "Go To Definition" 
+//            For Alphanumeric keys, the values are their ascii values (uppercase).
+// Input    : const KEY_EVENT_RECORD& keyboardEvent
+// Output   : void
+//--------------------------------------------------------------
+void gameplayKBHandler(const KEY_EVENT_RECORD& keyboardEvent)
+{
+    // here, we map the key to our enums
+    EKEYS key = K_COUNT;
+    switch (keyboardEvent.wVirtualKeyCode)
+    {
+    case VK_UP: key = K_UP; break;
+    case VK_DOWN: key = K_DOWN; break;
+    case VK_LEFT: key = K_LEFT; break; 
+    case VK_RIGHT: key = K_RIGHT; break; 
+    case VK_SPACE: key = K_SPACE; break;
+    case VK_ESCAPE: key = K_ESCAPE; break; 
+    }
+    // a key pressed event would be one with bKeyDown == true
+    // a key released event would be one with bKeyDown == false
+    // if no key is pressed, no event would be fired.
+    // so we are tracking if a key is either pressed, or released
+    if (key != K_COUNT)
+    {
+        g_skKeyEvent[key].keyDown = keyboardEvent.bKeyDown;
+        g_skKeyEvent[key].keyReleased = !keyboardEvent.bKeyDown;
+    }    
+}
+
+//--------------------------------------------------------------
+// Purpose  : This is the mouse handler in the game state. Whenever there is a mouse event in the game state, this function will be called.
+//            
+//            If mouse clicks are detected, the corresponding bit for that mouse button will be set.
+//            mouse wheel, 
+//            
+// Input    : const KEY_EVENT_RECORD& keyboardEvent
+// Output   : void
+//--------------------------------------------------------------
+void gameplayMouseHandler(const MOUSE_EVENT_RECORD& mouseEvent)
+{
+    if (mouseEvent.dwEventFlags & MOUSE_MOVED) // update the mouse position if there are no events
+    {
+        g_mouseEvent.mousePosition = mouseEvent.dwMousePosition;
+    }
+    g_mouseEvent.buttonState = mouseEvent.dwButtonState;
+    g_mouseEvent.eventFlags = mouseEvent.dwEventFlags;
 }
 
 //--------------------------------------------------------------
@@ -102,10 +208,63 @@ void update(double dt)
     {
         case S_SPLASHSCREEN : splashScreenWait(); // game logic for the splash screen
             break;
-        case S_GAME: gameplay(); // gameplay logic when we are in the game
+        case S_GAME: updateGame(); // gameplay logic when we are in the game
             break;
     }
 }
+
+
+void splashScreenWait()    // waits for time to pass in splash screen
+{
+    if (g_dElapsedTime > 3.0) // wait for 3 seconds to switch to game mode, else do nothing
+        g_eGameState = S_GAME;
+}
+
+void updateGame()       // gameplay logic
+{
+    processUserInput(); // checks if you should change states or do something else with the game, e.g. pause, exit
+    moveCharacter();    // moves the character, collision detection, physics, etc
+                        // sound can be played here too.
+}
+
+void moveCharacter()
+{    
+    // Updating the location of the character based on the key release
+    // providing a beep sound whenver we shift the character
+    if (g_skKeyEvent[K_UP].keyReleased && g_sChar.m_cLocation.Y > 0)
+    {
+        //Beep(1440, 30);
+        g_sChar.m_cLocation.Y--;       
+    }
+    if (g_skKeyEvent[K_LEFT].keyReleased && g_sChar.m_cLocation.X > 0)
+    {
+        //Beep(1440, 30);
+        g_sChar.m_cLocation.X--;        
+    }
+    if (g_skKeyEvent[K_DOWN].keyReleased && g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 1)
+    {
+        //Beep(1440, 30);
+        g_sChar.m_cLocation.Y++;        
+    }
+    if (g_skKeyEvent[K_RIGHT].keyReleased && g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1)
+    {
+        //Beep(1440, 30);
+        g_sChar.m_cLocation.X++;        
+    }
+    if (g_skKeyEvent[K_SPACE].keyReleased)
+    {
+        g_sChar.m_bActive = !g_sChar.m_bActive;        
+    }
+
+   
+}
+void processUserInput()
+{
+    // quits the game if player hits the escape key
+    if (g_skKeyEvent[K_ESCAPE].keyReleased)
+        g_bQuitGame = true;    
+}
+
 //--------------------------------------------------------------
 // Purpose  : Render function is to update the console screen
 //            At this point, you should know exactly what to draw onto the screen.
@@ -119,83 +278,26 @@ void render()
     clearScreen();      // clears the current screen and draw from scratch 
     switch (g_eGameState)
     {
-        case S_SPLASHSCREEN: renderSplashScreen();
-            break;
-        case S_GAME: renderGame();
-            break;
+    case S_SPLASHSCREEN: renderSplashScreen();
+        break;
+    case S_GAME: renderGame();
+        break;
     }
-    renderFramerate();  // renders debug information, frame rate, elapsed time, etc
-    renderToScreen();   // dump the contents of the buffer to the screen, one frame worth of game
-}
-
-void splashScreenWait()    // waits for time to pass in splash screen
-{
-    if (g_dElapsedTime > 3.0) // wait for 3 seconds to switch to game mode, else do nothing
-        g_eGameState = S_GAME;
-}
-
-void gameplay()            // gameplay logic
-{
-    processUserInput(); // checks if you should change states or do something else with the game, e.g. pause, exit
-    moveCharacter();    // moves the character, collision detection, physics, etc
-                        // sound can be played here too.
-}
-
-void moveCharacter()
-{
-    bool bSomethingHappened = false;
-    if (g_dBounceTime > g_dElapsedTime)
-        return;
-
-    // Updating the location of the character based on the key press
-    // providing a beep sound whenver we shift the character
-    if (g_abKeyPressed[K_UP] && g_sChar.m_cLocation.Y > 0)
-    {
-        //Beep(1440, 30);
-        g_sChar.m_cLocation.Y--;
-        bSomethingHappened = true;
-    }
-    if (g_abKeyPressed[K_LEFT] && g_sChar.m_cLocation.X > 0)
-    {
-        //Beep(1440, 30);
-        g_sChar.m_cLocation.X--;
-        bSomethingHappened = true;
-    }
-    if (g_abKeyPressed[K_DOWN] && g_sChar.m_cLocation.Y < g_Console.getConsoleSize().Y - 1)
-    {
-        //Beep(1440, 30);
-        g_sChar.m_cLocation.Y++;
-        bSomethingHappened = true;
-    }
-    if (g_abKeyPressed[K_RIGHT] && g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1)
-    {
-        //Beep(1440, 30);
-        g_sChar.m_cLocation.X++;
-        bSomethingHappened = true;
-    }
-    if (g_abKeyPressed[K_SPACE])
-    {
-        g_sChar.m_bActive = !g_sChar.m_bActive;
-        bSomethingHappened = true;
-    }
-
-    if (bSomethingHappened)
-    {
-        // set the bounce time to some time in the future to prevent accidental triggers
-        g_dBounceTime = g_dElapsedTime + 0.125; // 125ms should be enough
-    }
-}
-void processUserInput()
-{
-    // quits the game if player hits the escape key
-    if (g_abKeyPressed[K_ESCAPE])
-        g_bQuitGame = true;    
+    renderFramerate();      // renders debug information, frame rate, elapsed time, etc
+    renderInputEvents();    // renders status of input events
+    renderToScreen();       // dump the contents of the buffer to the screen, one frame worth of game
 }
 
 void clearScreen()
 {
     // Clears the buffer with this colour attribute
     g_Console.clearBuffer(0x1F);
+}
+
+void renderToScreen()
+{
+    // Writes the buffer to the console, hence you will see what you have written
+    g_Console.flushBufferToConsole();
 }
 
 void renderSplashScreen()  // renders the splash screen
@@ -265,8 +367,82 @@ void renderFramerate()
     c.Y = 0;
     g_Console.writeToBuffer(c, ss.str(), 0x59);
 }
-void renderToScreen()
+
+// this is an example of how you would use the input events
+void renderInputEvents()
 {
-    // Writes the buffer to the console, hence you will see what you have written
-    g_Console.flushBufferToConsole();
+    // keyboard events
+    COORD startPos = {50, 2};
+    std::ostringstream ss;
+    std::string key;
+    for (int i = 0; i < K_COUNT; ++i)
+    {
+        ss.str("");
+        switch (i)
+        {
+        case K_UP: key = "UP";
+            break;
+        case K_DOWN: key = "DOWN";
+            break;
+        case K_LEFT: key = "LEFT";
+            break;
+        case K_RIGHT: key = "RIGHT";
+            break;
+        case K_SPACE: key = "SPACE";
+            break;
+        default: continue;
+        }
+        if (g_skKeyEvent[i].keyDown)
+            ss << key << " pressed";
+        else if (g_skKeyEvent[i].keyReleased)
+            ss << key << " released";
+        else
+            ss << key << " not pressed";
+
+        COORD c = { startPos.X, startPos.Y + i };
+        g_Console.writeToBuffer(c, ss.str(), 0x17);
+    }
+
+    // mouse events    
+    ss.str("");
+    ss << "Mouse position (" << g_mouseEvent.mousePosition.X << ", " << g_mouseEvent.mousePosition.Y << ")";
+    g_Console.writeToBuffer(g_mouseEvent.mousePosition, ss.str(), 0x59);
+    ss.str("");
+    switch (g_mouseEvent.eventFlags)
+    {
+    case 0:
+        if (g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+        {
+            ss.str("Left Button Pressed");
+            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 1, ss.str(), 0x59);
+        }
+        else if (g_mouseEvent.buttonState == RIGHTMOST_BUTTON_PRESSED)
+        {
+            ss.str("Right Button Pressed");
+            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 2, ss.str(), 0x59);
+        }
+        else
+        {
+            ss.str("Some Button Pressed");
+            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 3, ss.str(), 0x59);
+        }
+        break;
+    case DOUBLE_CLICK:
+        ss.str("Double Clicked");
+        g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 4, ss.str(), 0x59);
+        break;        
+    case MOUSE_WHEELED:
+        if (g_mouseEvent.buttonState & 0xFF000000)
+            ss.str("Mouse wheeled down");
+        else
+            ss.str("Mouse wheeled up");
+        g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 5, ss.str(), 0x59);
+        break;
+    default:        
+        break;
+    }
+    
 }
+
+
+
--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -6,6 +6,23 @@
 extern CStopWatch g_swTimer;
 extern bool g_bQuitGame;
 
+// struct to store keyboard events
+// a small subset of KEY_EVENT_RECORD
+struct SKeyEvent
+{
+    bool keyDown;
+    bool keyReleased;
+};
+
+// struct to store mouse events
+// a small subset of MOUSE_EVENT_RECORD
+struct SMouseEvent
+{
+    COORD mousePosition;
+    DWORD buttonState;
+    DWORD eventFlags;
+};
+
 // Enumeration to store the control keys that your game will have
 enum EKEYS
 {
@@ -40,7 +57,7 @@ void render      ( void );      // renders the current state of the game to the 
 void shutdown    ( void );      // do clean up, free memory
 
 void splashScreenWait();    // waits for time to pass in splash screen
-void gameplay();            // gameplay logic
+void updateGame();          // gameplay logic
 void moveCharacter();       // moves the character, collision detection, physics, etc
 void processUserInput();    // checks if you should change states or do something else with the game, e.g. pause, exit
 void clearScreen();         // clears the current screen and draw from scratch 
@@ -50,5 +67,14 @@ void renderMap();           // renders the map to the buffer first
 void renderCharacter();     // renders the character into the buffer
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
+void renderInputEvents();   // renders the status of input events
+
+// keyboard and mouse input event managers
+void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent);  // define this function for the console to call when there are keyboard events
+void mouseHandler(const MOUSE_EVENT_RECORD& mouseEvent);      // define this function for the console to call when there are mouse events
+
+void gameplayKBHandler(const KEY_EVENT_RECORD& keyboardEvent);   // handles keyboard events for gameplay 
+void gameplayMouseHandler(const MOUSE_EVENT_RECORD& mouseEvent); // handles mouse events for gameplay 
+
 
 #endif // _GAME_H
