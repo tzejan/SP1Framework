5d2fff93a760e1ffed5b85152bb461db9b9369fc
Wittez <69459645+Wittez@users.noreply.github.com>
2020-08-17 15:27:04 +0800

    Changed variable type for itemtag in Shelf class files

--- SP1Framework/Shelf.cpp
+++ SP1Framework/Shelf.cpp
@@ -23,12 +23,12 @@ void Shelf::setAmount(int amount)
     this->amount = amount;
 }
 
-signed char Shelf::getItemtag()
+int Shelf::getItemtag()
 {
     return itemtag;
 }
 
-void Shelf::setItemtag(signed char itemtag)
+void Shelf::setItemtag(int itemtag)
 {
     this->itemtag = itemtag;
 }
--- SP1Framework/Shelf.h
+++ SP1Framework/Shelf.h
@@ -6,7 +6,7 @@ class Shelf :
 private:
     const int capacity;//Capacity of the shelf
     int amount;// Current amount on shelf
-    signed char itemtag;//Type of item the shelf will have
+    int itemtag;//Type of item the shelf will have
 
 public:
     Shelf();
@@ -14,7 +14,7 @@ public:
     int getCapacity();
     int getAmount();
     void setAmount(int amount);
-    signed char getItemtag();
-    void setItemtag(signed char itemtag);
+    int getItemtag();
+    void setItemtag(int itemtag);
 };
 
