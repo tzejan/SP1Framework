a243c636235e71cd56530baa88f00a94d0f994ae
number10203 <69341783+number10203@users.noreply.github.com>
2020-08-19 11:02:10 +0800

    Position

---
+++ SP1Framework/Position.h
@@ -0,0 +1,18 @@
+#pragma once
+class Position
+{
+private:
+	int x, y;
+
+public:
+	Position();
+	~Position();
+
+	void setXpos(int xPos);
+	int getXpos();
+
+	void setYpos(int Ypos);
+	int getYpos();
+
+
+};
