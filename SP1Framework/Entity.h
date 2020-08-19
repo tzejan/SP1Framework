b1aa1b43cd2951bd1059b2803088d01ad25dc93c
hqgitter <69576378+hqgitter@users.noreply.github.com>
2020-08-19 14:46:40 +0800

    Edited Existing Entity class
    Utilises COORD
    
    move function to be done elsewhere

--- SP1Framework/Entity.h
+++ SP1Framework/Entity.h
@@ -1,22 +1,30 @@
 #pragma once
+#include <windows.h>
+
 
 class Entity
 {
 private:
-	int x_pos;
-	int y_pos;
+	COORD c;
 	char display;
 public:
+	Entity();
+	Entity(COORD _c, char _display);
 	Entity(int y_pos, int x_pos, char display);
+	
+	
+	//Get COORD or X and Y pos separately
+	COORD get_pos();
 	int get_x_pos();
 	int get_y_pos();
+	
+	//Get Entity's symbol
 	char get_display();
+
+	//Set COORD or X and Y pos separately
+	void set_pos(COORD _c);
 	void set_ypos(int _y_pos);
 	void set_xpos(int _x_pos);
-	void move_up(int distance);
-	void move_down(int distance);
-	void move_left(int distance);
-	void move_right(int distance);
-
+	
 
 };
--- SP1Framework/entity.cpp
+++ SP1Framework/entity.cpp
@@ -1,21 +1,39 @@
 #include "Entity.h"
 
+Entity::Entity()
+{
+	c.X = 0;
+	c.Y = 0;
+	display = ' ';
+}
+
+Entity::Entity(COORD _c, char _display)
+{
+	c = _c;
+	display = _display;
+}
+
 Entity::Entity(int _y_pos, int _x_pos, char _display)
 {
-	y_pos = _y_pos;
-	x_pos = _x_pos;
+	c.Y = _y_pos;
+	c.X = _x_pos;
 	display = _display;
 
 }
 
 int Entity::get_y_pos()
 {
-	return y_pos;
+	return c.Y;
+}
+
+COORD Entity::get_pos()
+{
+	return c;
 }
 
 int Entity::get_x_pos()
 {
-	return x_pos;
+	return c.X;
 }
 
 char Entity::get_display()
@@ -23,32 +41,19 @@ char Entity::get_display()
 	return display;
 }
 
+void Entity::set_pos(COORD _c)
+{
+	c = _c;
+}
+
 void Entity::set_ypos(int _y_pos)
 {
-	y_pos = _y_pos;
+	c.Y = _y_pos;
 }
 
 void Entity::set_xpos(int _x_pos)
 {
-	x_pos = _x_pos;
+	c.X = _x_pos;
 }
 
-void Entity::move_up(int distance)
-{
-	y_pos -= distance;
-}
 
-void Entity::move_down(int distance)
-{
-	y_pos += distance;
-}
-
-void Entity::move_left(int distance)
-{
-	x_pos -= distance;
-}
-
-void Entity::move_right(int distance)
-{
-	x_pos += distance;
-}
