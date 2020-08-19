209cffb92531ae16fcc2f93efe2f8dd04f1d0d06
Wittez <69459645+Wittez@users.noreply.github.com>
2020-08-19 17:07:00 +0800

    Updated text for main menu depending on previous state

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -293,6 +293,7 @@ void updateEndofWorkScreen()
 
 void updateHome() // Home logic
 {
+    g_ePreviousGameState = g_eGameState;
     processUserInput(); // checks if you should change states or do something else with the game, e.g. pause, exit
 }
 
@@ -639,6 +640,8 @@ void renderMainMenu()
     c.X = g_Console.getConsoleSize().X / 6 + 20;
     if (g_ePreviousGameState == S_SPLASHSCREEN)
         g_Console.writeToBuffer(c, "Start New", 0xF0);
+    else if (g_ePreviousGameState == S_HOME)
+        g_Console.writeToBuffer(c, "Back Home", 0xF0);
     else
         g_Console.writeToBuffer(c, "Resume Work", 0xF0);
     c.Y += 1;
