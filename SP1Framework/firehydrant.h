0530858e13dc88583e3cfee01527176e8c16d2c3
KneeGah <190874h@mymail.nyp.edu.sg>
2020-08-20 14:42:02 +0800

    Update firehydrant.h

--- SP1Framework/firehydrant.h
+++ SP1Framework/firehydrant.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Entities.h"
+#include <wincontypes.h>
 class firehydrant : public Entity
 {
 protected:
 	static int total_no_of_hydrant;
+	COORD position;
 public:
 	static void SetTotalHydrant(int SetTotalHr);
 	static int GetTotalHydrant();
